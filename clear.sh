9db8bd6c1300f243d09da39a625687c192062f54
root <root@hpc-sgp-grafana-1.48>
2025-06-09 20:23:14 +0800

    v3.0.11_t7_config

---
+++ clear.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf "license_coverage.txt"
+rm -rf "output" "dist"
+rm -rf behavior-test
+rm -rf performance-test
+rm -rf core-test
+rm -rf e2e-engine-coverage.txt
+rm -rf find_licenses
+rm -rf "generated_files"
+rm -rf .testCoverage.txt
+rm -rf .coretestCoverage.txt
+rm -rf core/build
+rm -rf plugin_main/*.dll
+rm -rf plugin_main/*.so
+rm -rf plugins/all/all.go
+rm -rf plugins/all/all_debug.go
+rm -rf plugins/all/all_windows.go
+rm -rf plugins/all/all_linux.go
