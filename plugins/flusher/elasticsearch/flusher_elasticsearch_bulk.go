e1d8b34e391fc1e588003781590daf969c015bad
root <root@hpc-sgp-grafana-1.48>
2025-06-20 11:54:44 +0800

    sdfsdf

--- plugins/flusher/elasticsearch/flusher_elasticsearch_bulk.go
+++ plugins/flusher/elasticsearch/flusher_elasticsearch_bulk.go
@@ -12,16 +12,19 @@ import (
 	"time"
 
 	"github.com/alibaba/ilogtail/pkg/fmtstr"
-	"github.com/alibaba/ilogtail/pkg/logger"
 	"github.com/alibaba/ilogtail/pkg/protocol"
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"gopkg.in/yaml.v3"
 )
 
 var (
-	totalDuration int64  // sendBulk函数消耗时间，累计，单位纳秒 -- 计算处理耗时
-	callCount     int64  // 调用 sendBulk 次数 -- 计算处理耗时
-	allBufCount   uint64 // buf 写入channel 计数 -- 计算写入速率为 W（条/秒）
+	sendBulkTotalDura int64 // sendBulk函数消耗时间，累计，单位纳秒 -- 计算处理耗时
+	sendBulkCallCount int64 // 调用 sendBulk 次数 -- 计算处理耗时
+	//
+	allBufCount uint64 // buf 写入channel 计数 -- 计算写入速率为 W（条/秒）
+	//
+	// logBufferLen     int64
+	// esWriteBufferLen int64
 )
 
 func init() {
@@ -36,33 +39,33 @@ func performanceLog() {
 	var old_allBufCount uint64
 	for range ticker.C {
 		new_totalDuration, new_callCount, new_allBufCount :=
-			atomic.LoadInt64(&totalDuration), atomic.LoadInt64(&callCount), atomic.LoadUint64(&allBufCount)
+			atomic.LoadInt64(&sendBulkTotalDura), atomic.LoadInt64(&sendBulkCallCount), atomic.LoadUint64(&allBufCount)
 
 		dura_60 := new_totalDuration - old_totalDuration
 		count_60 := new_callCount - old_callCount
+		//
 		allBufCount_60 := new_allBufCount - old_allBufCount
-
 		// 每次请求耗时
 		var sendBulkDura int64
-		if count_60 < 1 {
-			sendBulkDura = 0
-		} else {
+		if count_60 > 0 {
 			sendBulkDura = dura_60 / count_60
 		}
-		log.Printf("--- buf chan write [ %d ] - write rate: [ %d/s ] - bulk write dura [ %s ] - chan read rate [ %d/s ] - bufChan[ %d/%d ] - min thread [ %d ].\n",
+		log.Printf("--- INFO buf chan write [ %d ] - write rate: [ %.3f/s ] - bulk write dura [ %s ] "+
+			"- chan read rate [ %.3f/s ] - bufChan[ %d/%d ] - min thread [ %.3f ].\n",
 			new_allBufCount,
-			allBufCount_60/60,
+			float64(allBufCount_60)/60,
 			time.Duration(sendBulkDura).String(),
-			count_60/60,
+			float64(count_60)/60,
 			len(bufChan),
 			cap(bufChan),
-			int64(allBufCount_60)*sendBulkDura/60/1e9,
+			float64(allBufCount_60)*float64(sendBulkDura)/60/1e9,
 		)
 		old_totalDuration, old_callCount, old_allBufCount = new_totalDuration, new_callCount, new_allBufCount
 	}
 }
 
 var bulkconf = getConfig()
+var maxBatchBytes = getMaxBatchBytes()
 
 type BlukConfig struct {
 	EsBlukConfig GoroutineConf `yaml:"es_bulk_config"`
@@ -76,9 +79,28 @@ type GoroutineConf struct {
 func getBufChanSize() (ret int) {
 	ret = bulkconf.EsBlukConfig.BulkBufChanSize
 	if ret < 3 {
-		ret = 100
+		ret = 500
 	}
+	log.Println("es buf chan size:", ret)
 	return
+}
+
+func getGoroutineSize() (ret int) {
+	ret = bulkconf.EsBlukConfig.GoThreadNum
+	if ret < 2 {
+		ret = 15
+	}
+	log.Println("es bulk goroutine size:", ret)
+	return
+}
+
+func getMaxBatchBytes() int {
+	bsize := bulkconf.EsBlukConfig.BatchSizeMB
+	if bsize < 1 {
+		bsize = 5
+	}
+	log.Println("es bulk size(MB):", bsize)
+	return bsize * MB
 }
 
 // 读取配置文件信息
@@ -97,73 +119,55 @@ func getConfig() *BlukConfig {
 	return &config
 }
 
-var bufferPool = sync.Pool{
-	New: func() any {
-		return new(bytes.Buffer)
-	},
-}
-
-func getBuffer() *bytes.Buffer {
-	buf := bufferPool.Get().(*bytes.Buffer)
-	buf.Reset()
-	return buf
-}
-
-func putBuffer(buf *bytes.Buffer) {
-	bufferPool.Put(buf)
-}
-
 var bufChan = make(chan *bytes.Buffer, getBufChanSize())
-
-func (f *FlusherElasticSearch) handleBufChan() {
-	goThreadNum := bulkconf.EsBlukConfig.GoThreadNum
-	if goThreadNum < 2 {
-		goThreadNum = 10
-	}
-	log.Println("es bulk goroutine number:", goThreadNum)
-	for range goThreadNum {
-		go func() {
-			for v := range bufChan {
-				if err := f.sendBulk(v); err != nil {
-					logger.Errorf(f.context.GetRuntimeContext(), "FLUSHER_FLUSH_ALARM", "Bulk send failed: %s", err)
-				}
-				putBuffer(v)
-			}
-		}()
-	}
-}
 
 const (
 	MB int = 1024 * 1024 // 1MB
 )
 
-var maxBatchBytes = getMaxBatchSize()
-
-func getMaxBatchSize() int {
-	bsize := bulkconf.EsBlukConfig.BatchSizeMB
-	if bsize < 3 {
-		bsize = 10
-	}
-	log.Println("es bulk size(MB):", bsize)
-	return bsize * MB
+var bufferPool = sync.Pool{
+	New: func() any {
+		// 预先分配容量，避免每次扩容
+		return bytes.NewBuffer(make([]byte, 0, maxBatchBytes))
+	},
 }
 
-// var compressed bytes.Buffer
-// var gw = gzip.NewWriter(&compressed)
+func GetBuffer() *bytes.Buffer {
+	buf := bufferPool.Get().(*bytes.Buffer)
+	buf.Reset() // 清空旧数据
+	return buf
+}
+
+func PutBuffer(buf *bytes.Buffer) {
+	// 超过最大大小的 buffer 不放回池中，防止长期占用过多内存
+	if buf.Cap() > maxBatchBytes+1*MB {
+		log.Println("--- WARN 释放buffer")
+		return
+	}
+	buf.Reset()
+	bufferPool.Put(buf)
+}
+
+var goroutineSize = getGoroutineSize()
+
+func (f *FlusherElasticSearch) handleBufChan() {
+	for i := 0; i < goroutineSize; i++ {
+		for tbuf := range bufChan {
+			if err := f.sendBulk(tbuf); err != nil {
+				log.Println("--- ERROR Bulk send failed:", err.Error())
+			}
+			PutBuffer(tbuf)
+		}
+	}
+}
 
 func (f *FlusherElasticSearch) sendBulk(bulkBuf *bytes.Buffer) error {
 	start := time.Now()
 	defer func() {
-		atomic.AddInt64(&totalDuration, time.Since(start).Nanoseconds())
-		atomic.AddInt64(&callCount, 1)
+		atomic.AddInt64(&sendBulkTotalDura, time.Since(start).Nanoseconds())
+		atomic.AddInt64(&sendBulkCallCount, 1)
 	}()
-	// if _, err := io.Copy(gw, bulkBuf); err != nil {
-	// 	return err
-	// }
-	// gw.Close()
-
 	req := esapi.BulkRequest{
-		//Body: bytes.NewReader(compressed.Bytes()),
 		Body: bytes.NewReader(bulkBuf.Bytes()),
 		Header: http.Header{
 			"Content-Encoding": {"gzip"},
@@ -172,19 +176,16 @@ func (f *FlusherElasticSearch) sendBulk(bulkBuf *bytes.Buffer) error {
 	}
 	res, err := req.Do(context.Background(), f.esClient)
 	if err != nil {
-		logger.Error(f.context.GetRuntimeContext(), "FLUSHER_FLUSH_ALARM", "flush elasticsearch request fail, error", err)
+		log.Println("--- ERROR FLUSHER_FLUSH_ALARM", "flush elasticsearch request fail, error", err)
 		return err
 	}
 	defer res.Body.Close()
-	//清空 buffer 并复用 gzip.Writer
-	// compressed.Reset()
-	// gw.Reset(&compressed) // 复用同一个 gzip.Writer 实例
 
 	if res.StatusCode >= 400 && res.StatusCode <= 499 {
-		logger.Error(f.context.GetRuntimeContext(), "FLUSHER_FLUSH_ALARM", "flush elasticsearch request client error", res)
+		log.Println("--- ERROR FLUSHER_FLUSH_ALARM", "flush elasticsearch request client error", res)
 		return fmt.Errorf("err status returned: %v", res.Status())
 	} else if res.StatusCode >= 500 && res.StatusCode <= 599 {
-		logger.Error(f.context.GetRuntimeContext(), "FLUSHER_FLUSH_ALARM", "flush elasticsearch request server error", res)
+		log.Println("--- ERROR FLUSHER_FLUSH_ALARM", "flush elasticsearch request server error", res)
 		return fmt.Errorf("err status returned: %v", res.Status())
 	}
 	return nil
@@ -195,52 +196,53 @@ func (f *FlusherElasticSearch) Flush(projectName string, logstoreName string, co
 	if f.Action != "" {
 		bulkAction = f.Action
 	}
+	tmpBuf := GetBuffer()
+	var byteTmpLen int
 	nowTime := time.Now().Local()
 	for _, logGroup := range logGroupList {
-		logger.Debug(f.context.GetRuntimeContext(), "[LogGroup] topic", logGroup.Topic, "logstore", logGroup.Category, "logcount", len(logGroup.Logs), "tags", logGroup.LogTags)
+		//logger.Debug(f.context.GetRuntimeContext(), "[LogGroup] topic", logGroup.Topic, "logstore", logGroup.Category, "logcount", len(logGroup.Logs), "tags", logGroup.LogTags)
 		serializedLogs, values, err := f.converter.ToByteStreamWithSelectedFields(logGroup, f.indexKeys)
 		if err != nil {
-			logger.Error(f.context.GetRuntimeContext(), "FLUSHER_FLUSH_ALARM", "flush elasticsearch convert log fail, error", err)
+			log.Println("--- ERROR FLUSHER_FLUSH_ALARM", "flush elasticsearch convert log fail, error", err)
 			return err
 		}
-		bulkBuf := getBuffer()
-		var batchBytes int
-		for index, log := range serializedLogs.([][]byte) {
+		for idx, logData := range serializedLogs.([][]byte) {
 			esIndex := &f.Index
 			if f.isDynamicIndex {
-				valueMap := values[index]
+				valueMap := values[idx]
 				esIndex, err = fmtstr.FormatIndex(valueMap, f.Index, uint32(nowTime.Unix()))
 				if err != nil {
-					logger.Error(f.context.GetRuntimeContext(), "FLUSHER_FLUSH_ALARM", "ERROR flush elasticsearch format index fail, error", err)
+					log.Println("--- ERROR FLUSHER_FLUSH_ALARM", "ERROR flush elasticsearch format index fail, error", err)
 					return err
 				}
 			}
 			meta := []byte(`{"` + bulkAction + `": {"_index": "` + *esIndex + `"}}` + "\n")
-			log = append(log, "\n"...)
-			logLen := len(meta) + len(log)
+			logData = append(logData, "\n"...)
 			//
-			bulkBuf.Grow(logLen)
-			bulkBuf.Write(meta)
-			bulkBuf.Write(log)
+			byteTmpLen += len(meta) + len(logData)
 			//
-			batchBytes += logLen
-			if batchBytes >= maxBatchBytes {
-				// if err := f.sendBulk(&bulkBuf); err != nil {
-				// 	logger.Errorf(f.context.GetRuntimeContext(), "FLUSHER_FLUSH_ALARM", "Bulk send failed: %s", err)
-				// }
-				// bulkBuf.Reset()
-				bufChan <- bulkBuf
-				atomic.AddUint64(&allBufCount, 1) // 批量写入计数，用于统计需要多少线程
-				batchBytes = 0
+			tmpBuf.Write(meta)
+			tmpBuf.Write(logData)
+			//
+			if byteTmpLen >= maxBatchBytes {
+				select {
+				case bufChan <- tmpBuf:
+					atomic.AddUint64(&allBufCount, 1)
+				default:
+					log.Println("--- WARN bufChan full, drop batch")
+					PutBuffer(tmpBuf)
+				}
+				tmpBuf = GetBuffer() // --- 获取新的 buffer，开始新一轮累积
 			}
 		}
-		// Flush the remaining data
-		if bulkBuf.Len() > 0 {
-			// if err := f.sendBulk(&bulkBuf); err != nil {
-			// 	logger.Errorf(f.context.GetRuntimeContext(), "FLUSHER_FLUSH_ALARM", "Final bulk send failed: %s", err)
-			// }
-			bufChan <- bulkBuf
-			atomic.AddUint64(&allBufCount, 1) // 批量写入计数，用于统计需要多少线程
+	}
+	if tmpBuf.Len() > 0 {
+		select {
+		case bufChan <- tmpBuf:
+			atomic.AddUint64(&allBufCount, 1)
+		default:
+			log.Println("--- WARN bufChan full, drop batch")
+			PutBuffer(tmpBuf)
 		}
 	}
 	return nil
